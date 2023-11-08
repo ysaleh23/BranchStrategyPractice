5188b36ca98f56ec440be94631b1549d75d59649
ysaleh23 <ysaleh037@gmail.com>
2023-11-07 22:03:36 -0600

    Added more code lines in the Hat class.

--- src/featureBpkg/Hat.java
+++ src/featureBpkg/Hat.java
@@ -4,5 +4,7 @@ public class Hat {
     public static void main(String[] args) {
 
         System.out.println("I like to wear hats.");
+
+        System.out.println("My favorite hat color is gray");
     }
 }
