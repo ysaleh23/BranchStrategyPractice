580f65529db716574c1cafa2ae31e84094292478
ysaleh23 <ysaleh037@gmail.com>
2023-11-07 22:19:57 -0600

    Added extra lines of code to the Store class.

--- src/featureCpkg/Store.java
+++ src/featureCpkg/Store.java
@@ -4,6 +4,8 @@ public class Store {
 
     public static void main(String[] args) {
         System.out.println("My favorite store is, best buy.");
+
+        System.out.println("The store is where you go to buy items!");
     }
 
 }
