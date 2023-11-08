e6ef13be06f029d0949359ccd982056a1377ccf6
ysaleh23 <ysaleh037@gmail.com>
2023-11-07 21:28:27 -0600

    Created food and water class.

---
+++ src/featureApkge/Water.java
@@ -0,0 +1,12 @@
+package featureApkge;
+
+public class Water {
+
+    public static void main(String[] args) {
+        System.out.println("You should listen to Dr.Hande, and drink 3 liters of water per day.");
+
+        System.out.println("You should also, drink water. So you don't sleep during the video, and get screenshotted by" +
+                "your group members.");
+    }
+
+}
